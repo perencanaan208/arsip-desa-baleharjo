f6fde1eeb6b3ad3826068528616d83135cc84967
perencanaan208 <perencanaan208@gmail.com>
2026-05-13 22:06:20 +0700

    Implement showMenu function for dynamic content display
    Added a function to show and manage menu content visibility.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Web Arsip Desa Aktif");
+function showMenu(menuId, element){
+
+  // sembunyikan semua content
+  const contents = document.querySelectorAll('.content');
+  contents.forEach(content=>{
+    content.classList.remove('active-content');
+  });
+
+  // tampilkan content dipilih
+  document.getElementById(menuId).classList.add('active-content');
+
+  // hapus active menu
+  const menus = document.querySelectorAll('.menu li');
+  menus.forEach(menu=>{
+    menu.classList.remove('active');
+  });
+
+  // active menu baru
+  element.classList.add('active');
+}
